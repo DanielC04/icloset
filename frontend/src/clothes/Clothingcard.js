8afb77e2174b7929493b527c7ebd15a10d572b20
Daniel Cermann <daniel.cermann@gmx.de>
2022-06-18 23:32:55 +0200

    really gud tshirt pictures

--- frontend/src/clothes/Clothingcard.js
+++ frontend/src/clothes/Clothingcard.js
@@ -9,8 +9,18 @@ export default function Clothingcard(props) {
 
 	const [score, setScore] = useState((minScore + maxScore) / 2);
 
+	let filename = props.filename
+	if (!filename)
+		filename = 'tshirt.png'
+
+	const style = {
+		backgroundImage: `url(${filename})`
+	}
+
+	console.log(style)
+
 	return (
-		<div className={`clothing-card ${props.className}`}>
+		<div className={`clothing-card ${props.className}`} style={style}>
 			<span className='name-tag'>{props.name}</span>
 			<div className='rating'>
 				<div onClick={() => setScore(Math.min(maxScore, score + 1))}>+</div>
--- frontend/src/pages/Closetpage.js
+++ frontend/src/pages/Closetpage.js
@@ -9,15 +9,15 @@ export default function Closetpage() {
 	return (
 		<Page useContainer={true} className='closet-page'>
 			<Clothingcollection name="T-shirts">
-				<Clothingcard />
-				<Clothingcard />
-				<Clothingcard />
-				<Clothingcard />
+				<Clothingcard name="Nike" filename="tshirt.png" />
+				<Clothingcard name="Louis" filename="tshirt2.jpeg" />
+				<Clothingcard name="Gucci" filename="tshirt3.jpg" />
+				<Clothingcard name="Dior" filename="tshirt4.webp" />
 				<CreateClothing />
 			</Clothingcollection>
 			<Clothingcollection name="Hosen">
-				<Clothingcard />
-				<Clothingcard />
+				<Clothingcard name="Fendi"/>
+				<Clothingcard name="Prada"/>
 				<Clothingcard />
 				<Clothingcard />
 				<Clothingcard />
